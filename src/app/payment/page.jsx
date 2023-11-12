92ea411893ac9f72b59fb1dbd8e9159d095fa365
anvo <songun1120@gamil.com>
2023-11-12 16:37:58 +0900

    src/app/payment/page.jsx

--- src/app/payment/page.jsx
+++ src/app/payment/page.jsx
@@ -3,12 +3,11 @@
 import CheckoutWizard from '@/components/CheckoutWizard'
 import { savePaymentMethod } from '@/redux/slices/cartSlice'
 import { useRouter } from 'next/navigation'
-
-import React, { use, useEffect } from 'react'
+import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { useDispatch, useSelector } from 'react-redux'
 
-export default function PaymentPage() {
+export default function ShippingAddressPage() {
   const {
     handleSubmit,
     register,
@@ -18,6 +17,7 @@ export default function PaymentPage() {
   const router = useRouter()
   const dispatch = useDispatch()
   const { shippingAddress, paymentMethod } = useSelector((state) => state.cart)
+
   useEffect(() => {
     if (!shippingAddress.address) {
       return router.push('/shipping')
@@ -30,14 +30,6 @@ export default function PaymentPage() {
 
     router.push('/placeorder')
   }
-
-  const payments = [
-    'PayPal',
-    'Stripe',
-    'CashOnDelivery',
-    'KakaoPay',
-    'NaverPay',
-  ]
   return (
     <div>
       <CheckoutWizard activeStep={2} />
@@ -45,31 +37,30 @@ export default function PaymentPage() {
         className="mx-auto max-w-screen-md"
         onSubmit={handleSubmit(submitHandler)}
       >
-        <h1 className="mb-4 text-xl font-bold">Payment Method</h1>
-        {payments.map((payment) => (
+        <h1 className="mb-4 text-xl">Payment Method</h1>
+        {['PayPal', 'Stripe', 'CashOnDelivery'].map((payment) => (
           <div key={payment} className="mb-4">
             <input
               name="paymentMethod"
-              className="p-2 outline-none focusing:ring-0"
+              className="p-2 outline-none focus:ring-0"
               id={payment}
               type="radio"
               value={payment}
               {...register('paymentMethod', {
-                required: 'please select payment method',
+                required: 'Please select payment method',
               })}
             />
+
             <label className="p-2" htmlFor={payment}>
               {payment}
             </label>
           </div>
         ))}
         {errors.paymentMethod && (
-          <div className="text-red-500">{errors.paymentMethod.message}</div>
+          <div className="text-red-500 ">{errors.paymentMethod.message}</div>
         )}
         <div className="mb-4 flex justify-between">
-          <button type="submit" className="primary-button">
-            Next
-          </button>
+          <button className="primary-button">Next</button>
         </div>
       </form>
     </div>
